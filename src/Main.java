06c974d5ba7b52d4e2b715a4326314d9aab61024
Toghrul <Toghrul@DESKTOP-1GBRFLQ>
2016-12-25 05:35:51 -0800

    Commit1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import algorithms.SegmentTreeSum;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
--- src/algorithms/SegmentTreeSum.java
+++ src/algorithms/SegmentTreeSum.java
@@ -10,5 +10,28 @@ package algorithms;
  * @author Toghrul
  */
 public class SegmentTreeSum {
-    
+    public int height, maxsize;
+    public long T[];
+    public SegmentTreeSum(int[] a) {
+        height = (int)Math.ceil((Math.log(a.length) / Math.log(2)));
+        maxsize = (int)(2 * Math.pow(2, height) - 1);
+        T = new long[maxsize];
+        init(1, a, 0, a.length - 1);
+    }
+    public void init(int x, int[] a, int l, int r) {
+        if(l == r) {
+            T[x] = a[l];
+        }else {
+            int m = (l + r) >> 1;
+            init(x << 1, a, l, m);
+            init(x << 1 | 1, a, m + 1, r);
+            T[x] = T[x << 1] + T[x << 1 | 1];
+        }
+    }
+    public long getSum(int x, int l, int r, int ll, int rr) {
+        if(ll <= l && r <= rr) return T[x];
+        if(r < ll || rr < l) return 0;
+        int m = (l + r) >> 1;
+        return getSum(x << 1, l, m, ll, rr) + getSum(x << 1 | 1, m + 1, r, ll, rr);
+    }
 }
