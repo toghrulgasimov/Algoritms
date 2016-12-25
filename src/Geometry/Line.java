2ac11ccc56b9178946b132a508cd6d17ac656188
Toghrul <Toghrul@DESKTOP-1GBRFLQ>
2016-12-25 14:32:28 -0800

    Commit1

--- src/Geometry/Line.java
+++ src/Geometry/Line.java
@@ -3,16 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package algorithms;
+package Geometry;
 
 /**
  *
  * @author Toghrul
  */
 public class Line {
-        public algorithms.Point p1, p2;
+        public Geometry.Point p1, p2;
         public double a, b, c;
-        public Line(algorithms.Point p1, algorithms.Point p2) {
+        public Line(Geometry.Point p1, Geometry.Point p2) {
             this.p1 = p1;
             this.p2 = p2;
             a = p2.y - p1.y;
--- src/Geometry/Point.java
+++ src/Geometry/Point.java
@@ -1,4 +1,4 @@
-package algorithms;
+package Geometry;
 
 public class Point {
         public double x, y;
