6008c7ca590bcb51a2c89760a176fe784c9b3938
Toghrul <Toghrul@DESKTOP-1GBRFLQ>
2016-12-25 04:25:08 -0800

    Commit1

--- src/algorithms/DSU.java
+++ src/algorithms/DSU.java
@@ -10,7 +10,31 @@ package algorithms;
  * @author Toghrul
  */
 public class DSU {
-    public DSU() { 
-        
+    public int[] parent;
+    public int[] rank;
+    public int count;
+    public DSU(int n) { 
+        parent = new int[n];
+        rank = new int[n];
+        count = n;
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+    public int find(int x) {
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+    public void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if(x == y) return;
+        if(rank[x] > rank[y]) {
+            parent[y] = x;
+        }else {
+            parent[x] = y;
+            if(rank[x] == rank[y]) rank[y]++;
+        }
     }
 }
