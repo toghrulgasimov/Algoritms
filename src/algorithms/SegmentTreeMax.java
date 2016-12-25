3e06738d5cc278de86448c2c2f94eeda97c9ddd0
Toghrul <Toghrul@DESKTOP-1GBRFLQ>
2016-12-25 06:52:16 -0800

    Commit1

--- src/algorithms/SegmentTreeMax.java
+++ src/algorithms/SegmentTreeMax.java
@@ -35,6 +35,19 @@ public class SegmentTreeMax {
         int m = (l + r) >> 1;
         return Math.max(getMax(x << 1, l, m, ll, rr), getMax(x << 1 | 1, m + 1, r, ll, rr));
     }
+    public void update(int x, int l, int r, int i, int v) {
+        if(l == r && l == i) {
+            T[x] = v;
+            return;
+        }
+        int m = (l + r) >> 1;
+        if(l <= i && i <= m) update(x << 1, l, m, i, v);
+        else if(m + 1 <= i && i <= r)update(x << 1 | 1, m + 1, r, i, v);
+        T[x] = Math.max(T[x << 1], T[x << 1 | 1]);
+    }
+    public void update(int i, int v) {
+        update(1, 0, n - 1, i, v);
+    }
     public long getMax(int l, int r) {
         return getMax(1, 0, n - 1, l, r);
     }
